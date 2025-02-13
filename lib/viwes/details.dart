814281d7a82dc54abb2e4fc2b051031db7bc5877
IBRAHEM-SALH <ibrahemsaleh862@gmail.com>
2025-02-13 12:37:53 +0300

    Update details.dart

--- lib/viwes/details.dart
+++ lib/viwes/details.dart
@@ -9,13 +9,46 @@ class Details extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Product Details")),
-      // 1 title  text 
+      // 1 title  text
       // 2 image image . network
-      // 3 price text string 
-      // 4 description string 
+      // 3 price text string
+      // 4 description string
       // 5 category string
-      // 6 rating string 
-
+      // 6 rating string
+      body: Center(
+        child: Container(
+          padding: EdgeInsets.all(15),
+          child: Column(
+            children: [
+               Image.network(product.image ?? "NO image",
+                  width: 100, height: 100, fit: BoxFit.cover),
+              Text(product.title ?? "NO title",
+                  style: const TextStyle(fontSize: 16, color: Colors.orange)),
+              SizedBox(height: 10),
+            
+              Text(product.price?.toString() ?? "NOt found price ",
+                  style: const TextStyle(fontSize: 16)),
+              SizedBox(
+                height: 10,
+              ),
+              Text(product.description ?? "not found description",
+                  style: const TextStyle(fontSize: 16, color: Colors.orange)),
+              SizedBox(
+                height: 10,
+              ),
+              Text(product.category ?? " no category",
+                  style: const TextStyle(fontSize: 16, color: Colors.orange)),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                  product.rating?.rate?.toStringAsFixed(1) ??
+                      "not found rating",
+                  style: const TextStyle(fontSize: 16, color: Colors.orange)),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
